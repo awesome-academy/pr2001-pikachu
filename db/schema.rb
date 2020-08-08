e6205a7c1a6aa97c7cd82d03fe751b289c66f54e
anhvo1987 <anhvo1987@gmail.com>
2020-08-08 10:18:51 +0700

    run rubocop

--- db/schema.rb
+++ db/schema.rb
@@ -10,152 +10,151 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_22_094538) do
-
-  create_table "active_storage_attachments", force: :cascade do |t|
-    t.string "name", null: false
-    t.string "record_type", null: false
-    t.integer "record_id", null: false
-    t.integer "blob_id", null: false
-    t.datetime "created_at", null: false
-    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
-    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
+ActiveRecord::Schema.define(version: 20_200_722_094_538) do
+  create_table 'active_storage_attachments', force: :cascade do |t|
+    t.string 'name', null: false
+    t.string 'record_type', null: false
+    t.integer 'record_id', null: false
+    t.integer 'blob_id', null: false
+    t.datetime 'created_at', null: false
+    t.index ['blob_id'], name: 'index_active_storage_attachments_on_blob_id'
+    t.index %w[record_type record_id name blob_id], name: 'index_active_storage_attachments_uniqueness', unique: true
   end
 
-  create_table "active_storage_blobs", force: :cascade do |t|
-    t.string "key", null: false
-    t.string "filename", null: false
-    t.string "content_type"
-    t.text "metadata"
-    t.bigint "byte_size", null: false
-    t.string "checksum", null: false
-    t.datetime "created_at", null: false
-    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  create_table 'active_storage_blobs', force: :cascade do |t|
+    t.string 'key', null: false
+    t.string 'filename', null: false
+    t.string 'content_type'
+    t.text 'metadata'
+    t.bigint 'byte_size', null: false
+    t.string 'checksum', null: false
+    t.datetime 'created_at', null: false
+    t.index ['key'], name: 'index_active_storage_blobs_on_key', unique: true
   end
 
-  create_table "booking_tours", force: :cascade do |t|
-    t.integer "tour_detail_id", null: false
-    t.integer "user_id", null: false
-    t.integer "quantity"
-    t.float "price_total"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.integer "booking_status", default: 1, null: false
-    t.integer "payment_status", default: 0, null: false
-    t.index ["tour_detail_id"], name: "index_booking_tours_on_tour_detail_id"
-    t.index ["user_id"], name: "index_booking_tours_on_user_id"
+  create_table 'booking_tours', force: :cascade do |t|
+    t.integer 'tour_detail_id', null: false
+    t.integer 'user_id', null: false
+    t.integer 'quantity'
+    t.float 'price_total'
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
+    t.integer 'booking_status', default: 1, null: false
+    t.integer 'payment_status', default: 0, null: false
+    t.index ['tour_detail_id'], name: 'index_booking_tours_on_tour_detail_id'
+    t.index ['user_id'], name: 'index_booking_tours_on_user_id'
   end
 
-  create_table "categories", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+  create_table 'categories', force: :cascade do |t|
+    t.string 'name'
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
   end
 
-  create_table "comments", force: :cascade do |t|
-    t.integer "review_id", null: false
-    t.integer "user_id", null: false
-    t.text "content"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["review_id"], name: "index_comments_on_review_id"
-    t.index ["user_id"], name: "index_comments_on_user_id"
+  create_table 'comments', force: :cascade do |t|
+    t.integer 'review_id', null: false
+    t.integer 'user_id', null: false
+    t.text 'content'
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
+    t.index ['review_id'], name: 'index_comments_on_review_id'
+    t.index ['user_id'], name: 'index_comments_on_user_id'
   end
 
-  create_table "images", force: :cascade do |t|
-    t.integer "tour_id", null: false
-    t.string "link"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["tour_id"], name: "index_images_on_tour_id"
+  create_table 'images', force: :cascade do |t|
+    t.integer 'tour_id', null: false
+    t.string 'link'
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
+    t.index ['tour_id'], name: 'index_images_on_tour_id'
   end
 
-  create_table "likes", force: :cascade do |t|
-    t.integer "review_id", null: false
-    t.integer "user_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["review_id"], name: "index_likes_on_review_id"
-    t.index ["user_id"], name: "index_likes_on_user_id"
+  create_table 'likes', force: :cascade do |t|
+    t.integer 'review_id', null: false
+    t.integer 'user_id', null: false
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
+    t.index ['review_id'], name: 'index_likes_on_review_id'
+    t.index ['user_id'], name: 'index_likes_on_user_id'
   end
 
-  create_table "payments", force: :cascade do |t|
-    t.integer "user_id", null: false
-    t.integer "coin"
-    t.integer "status", default: 0, null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.integer "tour_id"
-    t.index ["user_id"], name: "index_payments_on_user_id"
+  create_table 'payments', force: :cascade do |t|
+    t.integer 'user_id', null: false
+    t.integer 'coin'
+    t.integer 'status', default: 0, null: false
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
+    t.integer 'tour_id'
+    t.index ['user_id'], name: 'index_payments_on_user_id'
   end
 
-  create_table "ratings", force: :cascade do |t|
-    t.integer "tour_id", null: false
-    t.integer "user_id", null: false
-    t.integer "rate"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["tour_id"], name: "index_ratings_on_tour_id"
-    t.index ["user_id"], name: "index_ratings_on_user_id"
+  create_table 'ratings', force: :cascade do |t|
+    t.integer 'tour_id', null: false
+    t.integer 'user_id', null: false
+    t.integer 'rate'
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
+    t.index ['tour_id'], name: 'index_ratings_on_tour_id'
+    t.index ['user_id'], name: 'index_ratings_on_user_id'
   end
 
-  create_table "reviews", force: :cascade do |t|
-    t.integer "tour_id", null: false
-    t.integer "user_id", null: false
-    t.text "content"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.text "picture"
-    t.index ["tour_id"], name: "index_reviews_on_tour_id"
-    t.index ["user_id"], name: "index_reviews_on_user_id"
+  create_table 'reviews', force: :cascade do |t|
+    t.integer 'tour_id', null: false
+    t.integer 'user_id', null: false
+    t.text 'content'
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
+    t.text 'picture'
+    t.index ['tour_id'], name: 'index_reviews_on_tour_id'
+    t.index ['user_id'], name: 'index_reviews_on_user_id'
   end
 
-  create_table "tour_details", force: :cascade do |t|
-    t.integer "tour_id", null: false
-    t.date "departure_time"
-    t.date "return_time"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["tour_id"], name: "index_tour_details_on_tour_id"
+  create_table 'tour_details', force: :cascade do |t|
+    t.integer 'tour_id', null: false
+    t.date 'departure_time'
+    t.date 'return_time'
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
+    t.index ['tour_id'], name: 'index_tour_details_on_tour_id'
   end
 
-  create_table "tours", force: :cascade do |t|
-    t.string "name"
-    t.text "description"
-    t.integer "price"
-    t.integer "category_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.integer "coupon"
-    t.integer "seats"
-    t.index ["category_id"], name: "index_tours_on_category_id"
+  create_table 'tours', force: :cascade do |t|
+    t.string 'name'
+    t.text 'description'
+    t.integer 'price'
+    t.integer 'category_id', null: false
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
+    t.integer 'coupon'
+    t.integer 'seats'
+    t.index ['category_id'], name: 'index_tours_on_category_id'
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.string "email"
-    t.string "password_digest"
-    t.string "remember_digest"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.boolean "admin", default: false
-    t.integer "coin", default: 0, null: false
-    t.index ["email"], name: "index_users_on_email", unique: true
+  create_table 'users', force: :cascade do |t|
+    t.string 'name'
+    t.string 'email'
+    t.string 'password_digest'
+    t.string 'remember_digest'
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
+    t.boolean 'admin', default: false
+    t.integer 'coin', default: 0, null: false
+    t.index ['email'], name: 'index_users_on_email', unique: true
   end
 
-  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
-  add_foreign_key "booking_tours", "tour_details"
-  add_foreign_key "booking_tours", "users"
-  add_foreign_key "comments", "reviews"
-  add_foreign_key "comments", "users"
-  add_foreign_key "images", "tours"
-  add_foreign_key "likes", "reviews"
-  add_foreign_key "likes", "users"
-  add_foreign_key "payments", "users"
-  add_foreign_key "ratings", "tours"
-  add_foreign_key "ratings", "users"
-  add_foreign_key "reviews", "tours"
-  add_foreign_key "reviews", "users"
-  add_foreign_key "tour_details", "tours"
-  add_foreign_key "tours", "categories"
+  add_foreign_key 'active_storage_attachments', 'active_storage_blobs', column: 'blob_id'
+  add_foreign_key 'booking_tours', 'tour_details'
+  add_foreign_key 'booking_tours', 'users'
+  add_foreign_key 'comments', 'reviews'
+  add_foreign_key 'comments', 'users'
+  add_foreign_key 'images', 'tours'
+  add_foreign_key 'likes', 'reviews'
+  add_foreign_key 'likes', 'users'
+  add_foreign_key 'payments', 'users'
+  add_foreign_key 'ratings', 'tours'
+  add_foreign_key 'ratings', 'users'
+  add_foreign_key 'reviews', 'tours'
+  add_foreign_key 'reviews', 'users'
+  add_foreign_key 'tour_details', 'tours'
+  add_foreign_key 'tours', 'categories'
 end
